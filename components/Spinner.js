857bfdf7c665215f2ea51229575879844fb7279c
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2024-08-29 21:33:25 +0530

    Add files via upload

---
+++ components/Spinner.js
@@ -0,0 +1,18 @@
+import styles from './Spinner.module.css';
+
+const Spinner = () => {
+  return (
+    <div className={styles.spinner}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default Spinner;
